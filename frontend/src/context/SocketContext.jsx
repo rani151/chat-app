e28a596e95cec63916d4826954e835dae235f85a
rani151 <2003010165@ipec.org.in>
2024-05-26 03:35:16 +0530

    socketio updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://localhost:5000", {
+			const socket = io("https://chat-app-production-bpsa.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
